a706bd6aafdaa762b95c805c4debfb5c2095ff6c
canhnd58 <canhnd_58@vnu.edu.vn>
2015-10-06 21:15:15 +0700

    Fix errors when creating new questions and answers in Admin page  - Add permit_params to enable editting  - Include creating answers while creating questions

--- quizapp_server/app/models/question.rb
+++ quizapp_server/app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :category
   has_many :answers, dependent: :destroy
+  accepts_nested_attributes_for :answers, allow_destroy: true
 
   validates :description, :kind, :score, presence: true
   validates :score, numericality: {greater_than: 0}
