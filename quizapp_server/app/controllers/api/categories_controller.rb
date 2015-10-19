9d263e21008d1301539dc20a57ca0ebc9a993ae9
canhnd58 <canhnd_58@vnu.edu.vn>
2015-10-19 19:59:06 +0700

    Fix error when client can't send post request

--- quizapp_server/app/controllers/api/categories_controller.rb
+++ quizapp_server/app/controllers/api/categories_controller.rb
@@ -1,6 +1,6 @@
 class Api::CategoriesController < ApplicationController
   def index
     @categories = Category.all
-    render json: @categories
+    render json: {'status': '200', 'data': @categories}
   end
 end
