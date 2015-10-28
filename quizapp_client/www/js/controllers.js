218aa38a2c3027baec1825136cf7a5a402a3a5a5
lehainam125 <dealig29@gmail.com>
2015-10-28 23:16:13 +0700

    Update client only process when server sent 200 status

--- quizapp_client/www/js/controllers.js
+++ quizapp_client/www/js/controllers.js
@@ -7,6 +7,8 @@ angular.module('starter.controllers', [])
   $scope.profile = function() {
     $state.go('profile');
   };
+
+  // Logout facebook
   $scope.logout = function() {
     ngFB.logout();
     $http({
@@ -16,11 +18,16 @@ angular.module('starter.controllers', [])
         access_token: facebook.getToken()
       }
     }).then(function successCallBack(response) {
-      console.log("Logout data: ", response);
+      if (response.data.status === 200) {
+        console.log("Logout data: ", response.data);
+        $state.go('login');
+      } else {
+        alert('Something went wrong!!!\nError: ' + response.data.status);
+        // do smt
+      }
     }, function errorCallBack(response) {
-      console.log("Something went wrong when Logout");
+      alert("Something went wrong when Logout");
     });
-    $state.go('login');
   };
 })
 
@@ -37,28 +44,36 @@ angular.module('starter.controllers', [])
       pointDotRadius: 2
     }
   };
-
+  // Get user score and other information
   $http({
     method: 'GET',
     url: 'api/users/' + facebook.getUserId()
   }).then(function successCallBack(response) {
-    console.log('Profile data: ', response.data.data);
-    $scope.user = response.data.data;
-    $scope.chart.data = [[$scope.user.accuracy, $scope.user.speed, $scope.user.versatility,
-                          $scope.user.impressiveness, $scope.user.diligence]];
+    if (response.data.status === 200) {
+      console.log('Profile data: ', response.data.data);
+      $scope.user = response.data.data;
+      $scope.chart.data = [[$scope.user.accuracy, $scope.user.speed, $scope.user.versatility,
+                            $scope.user.impressiveness, $scope.user.diligence]];
+    } else {
+      alert('Something went wrong!!!\nError: ' + response.data.status);
+      // do smt
+    }
   }, function errorCallBack(response) {
-    console.log("Something went wrong");
+    alert("Something went wrong!!!");
   });
 })
 
 .controller('loginController', function($scope, $state, $http, ngFB, facebook) {
+  // ngLoginfb
   $scope.fbLogin = function() {
     ngFB.login({scope: ''}).then(
       function(response) {
         if (response.status === 'connected') {
           console.log('Facebook login in browser succeeded');
+          // Get accessToken from facebook
           var accessToken = response.authResponse.accessToken;
           facebook.setToken(accessToken);
+          // Send accessToken to server
           $http({
             method: 'POST',
             url: '/api/users/session',
@@ -67,10 +82,13 @@ angular.module('starter.controllers', [])
               access_token: accessToken
             }
           }).then(function successCallBack(response) {
-            console.log('AcessToken sent successfully', status, response);
-            console.log('User ID: ', response.data.data.id);
-            facebook.setUserId(response.data.data.id);
-            $state.go('menu');
+            if (response.data.status === 200) {
+              facebook.setUserId(response.data.data.id);
+              $state.go('menu');
+            } else {
+              alert('Something went wrong!!!\nError: ' + response.data.status);
+              // do smt
+            }
           }, function errorCallBack(response) {
             alert('Facebook login failed');
           });
@@ -81,21 +99,29 @@ angular.module('starter.controllers', [])
 })
 
 .controller('categoriesController', function($scope, $http, $state, categoryId, facebook, ionicMaterialInk, ionicMaterialMotion) {
-  $http.get('/api/categories')
-  // $http.get('http://se2015-quizapp.herokuapp.com/api/categories')
-    .success(function(data) {
-      console.log('Categories Success', data);
-      $scope.categoriesData = data.data;
-    })
-    .error(function(data) {
-      console.log('Something went wrong when get categories', data);
-    });
+  // Get Categories from server
+  $http({
+    method: 'GET',
+    url: 'api/categories',
+  }).then(function successCallBack(response) {
+    if (response.data.status === 200) {
+      console.log('Categories Success', response.data);
+      $scope.categoriesData = response.data.data;
+    } else {
+      alert('Something went wrong!!!\nError: ' + response.data.status);
+      // do smt
+    }
+  }, function errorCallBack(response) {
+    alert('Something went wrong!!!');
+  });
 
+  //
   $scope.takeQuiz = function(idValue) {
     categoryId.setId(idValue);
     $state.go('questions');
   };
 
+  //
   $scope.goProfile = function() {
     $state.go('profile');
   };
@@ -106,6 +132,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('questionsController', function($scope, $http, categoryId, facebook, ionicMaterialInk, ionicMaterialMotion) {
+  // Get list of questions of user category choice
   $http({
     method: 'GET',
     url:'/api/match',
@@ -114,11 +141,16 @@ angular.module('starter.controllers', [])
       access_token: facebook.getToken()
     }
   }).then(function successCallBack(response) {
-    console.log("Get questions succeeded", response.data.data.questions);
-    $scope.clientData = response.data.data.questions;
-    $scope.clientSideList = response.data.data.questions[0];
+    if (response.data.status === 200) {
+      console.log("Get questions succeeded", response.data);
+      $scope.clientData = response.data.data.questions;
+      $scope.clientSideList = response.data.data.questions[0];
+    } else {
+      alert('Something went wrong!!!\nError: ' + response.data.status);
+      // do smt
+    }
   }, function errorCallBack(response) {
-    console.log("Something went wrong");
+    alert("Something went wrong!!!");
   });
 
   $scope.cnt = 0;
