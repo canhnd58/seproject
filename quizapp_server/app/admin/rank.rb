17687fd914482a9cf69ef3a4d7de8da419a8d41b
canhnd58 <canhnd_58@vnu.edu.vn>
2015-12-07 20:56:25 +0700

    Fix error when uploading rank's avatar

--- quizapp_server/app/admin/rank.rb
+++ quizapp_server/app/admin/rank.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Rank do
   permit_params :avatar
+
+  show do
+    attributes_table do
+      row :id
+      row "Avatar" do |rank|
+        link_to image_tag(rank.avatar.url), rank.avatar.url
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
 end
--- quizapp_server/app/uploaders/rank_avatar_uploader.rb
+++ quizapp_server/app/uploaders/rank_avatar_uploader.rb
@@ -2,15 +2,15 @@
 
 class RankAvatarUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
-  WIDTH = 300
-  HEIGHT = 300
+  WIDTH = 128
+  HEIGHT = 128
 
   process eager: true
   process resize_to_fill: [WIDTH, HEIGHT]
   process convert: 'png'
 
   def public_id
-    'rank' + model.id
+    'rank' + model.id.to_s
   end
 
   def extension_white_list
